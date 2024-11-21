223a7a25e5ccb22491941d9b7e7bcec59ade3467
salekelfahim <salekelfahim@gmail.com>
2024-11-21 12:01:11 +0100

    Adding necesssary valadition to the entity

--- src/main/java/com/example/citronix/domain/Tree.java
+++ src/main/java/com/example/citronix/domain/Tree.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Entity
@@ -23,5 +24,34 @@ public class Tree {
 
     @OneToMany(mappedBy = "tree", cascade = CascadeType.ALL)
     private List<HarvestDetail> harvestDetails;
+
+    public int getAge() {
+        if (plantingDate != null) {
+            return Period.between(plantingDate, LocalDate.now()).getYears();
+        }
+        return 0;
+    }
+
+    public double getAnnualProductivity() {
+        int age = getAge();
+        if (age < 3) {
+            return 2.5;
+        } else if (age <= 10) {
+            return 12;
+        }
+        return 0;
+    }
+
+    public boolean isProductive() {
+        return getAge() <= 20;
+    }
+
+    public boolean isValidPlantingPeriod() {
+        if (plantingDate != null) {
+            int month = plantingDate.getMonthValue();
+            return month >= 3 && month <= 5;
+        }
+        return false;
+    }
 
 }
